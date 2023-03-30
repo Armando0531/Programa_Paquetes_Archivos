c8ba48b2469cf4a9c67f76f2f74a7caf9b7853c6
Jose Armando Enriquez Luna <armandoenriquez0531@gmail.com>
2023-03-30 16:26:19 -0600

    Se crearon los paquetes para las figuras de dos y tres dimensiones y se crearon las clases para cada figura

---
+++ src/edu/tecjerez/topicos/figuras/dosDimensiones/Elipse.java
@@ -0,0 +1,10 @@
+package edu.tecjerez.topicos.figuras.dosDimensiones;
+
+public class Elipse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/edu/tecjerez/topicos/figuras/dosDimensiones/Rombo.java
@@ -0,0 +1,10 @@
+package edu.tecjerez.topicos.figuras.dosDimensiones;
+
+public class Rombo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
